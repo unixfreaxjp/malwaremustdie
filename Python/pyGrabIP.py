0d729cbe22bfb301a618f6603518da93e0c7689b
malwaremustdie <malwaremustdie@3abfa918-fc25-f6f9-8a97-f7317e0f4758>
2012-10-20 15:26:20 +0000

    You need the following software packages stated in the below link in order to install Scapy if you are using Windows: http://www.secdev.org/projects/scapy/doc/installation.html#windows
    you need to have these 2 as well.
    http://appliedsec.github.com/pygeoip/
    http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz

---
+++ Python/pyGrabIP.py
@@ -0,0 +1,37 @@
+from scapy.all import *
+import operator
+
+# Main Program
+def main(fPcap):
+    packets = rdpcap(fPcap)
+    stats = {}
+    for packet in packets:
+        try:
+            dst = packet.payload.dst
+            if dst not in stats: stats[dst] = 0
+            stats[dst] += 1
+        except:
+            pass
+    for k,v in sorted(stats.iteritems(), key=operator.itemgetter(1))[::-1]:
+        import pygeoip
+        gi = pygeoip.GeoIP('.\\GeoIP\\GeoIP.dat')
+        print k
+        print "GeoIP: %s" % gi.country_code_by_addr(k)
+
+# Banner
+def Banner():
+    print("=================================================")
+    print("GrabIP from PCAP v0.1                            ")
+    print("=================================================")
+
+# Usage
+def help_menu(cmd):
+    print("Usage: %s <Name_of_PCAP_File>\n") % (cmd)
+
+if __name__ == '__main__':
+    Banner()
+    if len(sys.argv)<2:
+        help_menu(sys.argv[0])
+    else:
+        fPcap = sys.argv[1]
+        main(fPcap)
