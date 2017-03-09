7a0092a65f0eb18c5d27fe7d9987cb723e354327
unixfreaxjp <unixfreaxjp@users.noreply.github.com>
2017-03-09 16:10:09 +0900

    pyRedirector.py
    Coded by "Gunther"

---
+++ Python/pyRedirector.py
@@ -0,0 +1,12 @@
+import urllib2, httplib
+
+class RedirectHandler(urllib2.HTTPRedirectHandler):
+    def http_error_301(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
+        result.status = code
+        return result
+
+    def http_error_302(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        result.status = code
+        return result
